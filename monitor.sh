e66be31f59059eb10f3eb5791974d8e44337c8eb
NA-003-RO <webmailnarek@gmail.com>
2026-03-04 22:20:03 +0400

    Resolve merge conflict between RAM and Disk features

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-SYSTEM_STATS="System Check: Root Disk Available is $(df / | awk 'NR==2 {print $5}')"
-
+SYSTEM_STATS="System Check: RAM is $(free -h | awk '/Mem:/ {print $7}') | Disk is $(df / | awk 'NR==2 {print $5}')"
 
 echo $SYSTEM_STATS
